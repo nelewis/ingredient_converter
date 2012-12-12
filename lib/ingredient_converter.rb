fe667092e9c763617766c5d387116a2ee7765963
Nicole Lewis <lewis.nicole@gmail.com>
2012-12-11 17:30:11 -0800

    more rdocs

--- lib/ingredient_converter.rb
+++ lib/ingredient_converter.rb
@@ -5,17 +5,16 @@
     # == Synopsis
     # US to UK ingredient measurement converter
     # == Description
-    # It is my final project for 2012 Fall Ruby language class
+    # It is my final project for 2012 Fall Ruby language class, although it is still a work in progress as at 12.11.12
     # == Todo
     # === Upcoming Features
     # 0. Ability to convert a stick of butter to not just ounces but to grams and tablespoons.  Also planned are from other types of measure such as flour, sugar, treacle.
     # === Known Issues
-    # 0. Nothing yet as it is a stub :)
+    # 0. Nothing yet 
     # == References
-    # 0. <i>I made it up as I went along.</i>
+    # 0. Tim Myer
     # == License
     # This code is provided under the terms of the {MIT License.}[http://www.opensource.org/licenses/mit-license.php]
-    #
     #--
     # This code is the proprietary intellectual property of its authors. It is not intended for publication.
     #++
@@ -24,8 +23,8 @@
     #
 class USingredientConverter
 	# 
-	# * The Method convert returns "4 ounces"   
-	# * It is a stub as my face melted attempting to get a hash inside a hash value by the deadline
+	# * The method "convert" sole reponsibility is to convert the units-from parameter into the units-to parameter.
+	# * It will always return "4 ounces" as it is a stub as my face melted off attempting to get a hash inside a hash value by the deadline
 	# * I will make this more useful shortly
 	#
 	def convert(unitsfrom,unitsto)
